bda51797be73f68df70e3599e2f6afcd1453b67f
James Cool <jamescool@Jamess-MacBook-Pro.local>
2024-07-31 09:46:54 +1000

    fix getQueryStringValue function

--- src/util/query_string.ts
+++ src/util/query_string.ts
@@ -1,10 +1,12 @@
 export function getQueryStringValue(name: string) {
-  const query = window.location.search.substring(1);
-  const vars = query.split('&');
-  for (let i = 0; i < vars.length; i++) {
-    const pair = vars[i].split('=');
-    if (pair[0] === name) {
-      return decodeURIComponent(pair[1]);
+  if ((window as any) && typeof window !== 'undefined') {
+    const query = (window as any).location.search.substring(1);
+    const vars = query.split('&');
+    for (let i = 0; i < vars.length; i++) {
+      const pair = vars[i].split('=');
+      if (pair[0] === name) {
+        return decodeURIComponent(pair[1]);
+      }
     }
   }
   return undefined;
